42249ffdf52e184e2e6f8c68328e607d06b122f5
George South <george.south@podium.com>
2021-04-26 14:01:18 -0600

    policies added

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :schload, Schload.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :schload, SchloadWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4444],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
